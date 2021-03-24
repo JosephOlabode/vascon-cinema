5301f1da90b5f343bce9469c50f7f50b066f0d96
JosephOlabode <josepholabode17@gmail.com>
2021-03-24 21:13:04 +0000

    implement the show time repository class

---
+++ app/Repository/ShowTimeRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Repository;
+
+
+class ShowTimeRepository implements IShowtime
+{
+
+    public function getAllShowTime()
+    {
+        // TODO: Implement getAllShowTime() method.
+    }
+
+    public function getShowTimeById($id)
+    {
+        // TODO: Implement getShowTimeById() method.
+    }
+
+    public function storeShowTime($showTime)
+    {
+        // TODO: Implement storeShowTime() method.
+    }
+
+    public function updateShowTimeById($showTime, $id)
+    {
+        // TODO: Implement updateShowTimeById() method.
+    }
+
+    public function deleteShowTimeById($id)
+    {
+        // TODO: Implement deleteShowTimeById() method.
+    }
+}
