1ca4b22757ae83cd953cc7950de1cdaead0c3d78
JosephOlabode <josepholabode17@gmail.com>
2021-03-24 21:00:31 +0000

    created the method signature in the cinema interface

--- app/Repository/ICinema.php
+++ app/Repository/ICinema.php
@@ -7,4 +7,8 @@ namespace App\Repository;
 Interface ICinema
 {
     public function getAllCinema();
+    public function getMovieById($id);
+    public function storeMovie($movie);
+    public function updateMovieById($movie, $id);
+    public function deleteMovieById($id);
 }
