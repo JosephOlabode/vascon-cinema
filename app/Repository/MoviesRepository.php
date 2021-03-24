ca35778bdf894ac4cd066c154529a8235f42b688
JosephOlabode <josepholabode17@gmail.com>
2021-03-24 20:57:31 +0000

    implemented the get all movies method in the repository

--- app/Repository/MoviesRepository.php
+++ app/Repository/MoviesRepository.php
@@ -4,7 +4,26 @@
 namespace App\Repository;
 
 
-class MoviesRepository
-{
+use App\Models\Movies;
 
+class MoviesRepository implements IMovies
+{
+    public function getAllMovies()
+    {
+        $movies = Movies::all();
+
+        if($movies->count() == 0) {
+            return response()->json([
+                'title' => 'Vas Movies',
+                'message' => 'No movie is available',
+                'data' => []
+            ]);
+        }
+
+        return response()->json([
+            'title'=> 'Vas Movies',
+            'message' => 'Movies are available',
+            'data' => $movies
+        ]);
+    }
 }
