64d2d7e6979919ae7a632367adb31c9fea2363fa
JosephOlabode <josepholabode17@gmail.com>
2021-03-24 21:20:32 +0000

    implemented the Cinema model

--- app/Models/Cinema.php
+++ app/Models/Cinema.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Cinema extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'location'
+    ];
+
+    protected $guarded = [
+        'id', 'createdAt', 'updatedAt'
+    ];
+
+    protected $hidden = [
+        'createdAt', 'updatedAt'
+    ];
 }
