b6c186663ee08b1d3262d63f307c5f9e8281815f
JosephOlabode <josepholabode17@gmail.com>
2021-03-25 08:36:27 +0000

    implemented the movie respository

--- app/Repository/IMovies.php
+++ app/Repository/IMovies.php
@@ -4,11 +4,13 @@
 namespace App\Repository;
 
 
+use Illuminate\Http\Request;
+
 Interface IMovies
 {
     public function getAllMovies();
     public function getMovieById($id);
-    public function storeMovie($movie);
-    public function updateMovieById($movie, $id);
+    public function storeMovie(Request $request);
+    public function updateMovieById(Request $request, $id);
     public function deleteMovieById($id);
 }
--- app/Repository/MoviesRepository.php
+++ app/Repository/MoviesRepository.php
@@ -2,9 +2,10 @@
 
 
 namespace App\Repository;
-
+use Validator;
 
 use App\Models\Movies;
+use Illuminate\Http\Request;
 
 class MoviesRepository implements IMovies
 {
@@ -29,21 +30,112 @@ class MoviesRepository implements IMovies
 
     public function getMovieById($id)
     {
-        // TODO: Implement getMovieById() method.
+        $movie = Movies::findOrFail($id);
+        if($movie) {
+            return response()->json([
+                'title' => 'Vas Movies',
+                'message' => 'Movie is available',
+                'data' => $movie
+            ]);
+        }
+
+        return response()->json([
+            'title' => 'Vas Movies',
+            'message' => 'No movie found',
+            'data' => null
+        ]);
     }
 
-    public function storeMovie($movie)
+    public function storeMovie($request)
     {
-        // TODO: Implement storeMovie() method.
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'rating' => 'required',
+            'runningTime' => 'required',
+            'website' => 'required',
+            'releaseDate' => 'required',
+            'trailer' => 'required',
+            'dvdRelease' => 'required',
+            'synopsys' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'title' => 'Vas Movies',
+                'message' => $validator->errors(),
+                'data' => []
+            ], 422);
+        }
+
+        return response()->json([
+            'title' => 'Vas Movies',
+            'message' => 'Movie stored successfully',
+            'data' => []
+        ]);
     }
 
-    public function updateMovieById($movie, $id)
+    public function updateMovieById($request, $id)
     {
-        // TODO: Implement updateMovieById() method.
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'rating' => 'required',
+            'runningTime' => 'required',
+            'website' => 'required',
+            'releaseDate' => 'required',
+            'trailer' => 'required',
+            'dvdRelease' => 'required',
+            'synopsys' => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'title' => 'Vas Movies',
+                'message' => $validator->errors(),
+                'data' => []
+            ], 422);
+        }
+
+        $movie = Movies::findOrFail($id);
+        if($movie == null) {
+            return response()->json([
+                'title' => 'Vas Movies',
+                'message' => 'No movie found',
+                'data' => null
+            ], 404);
+        }
+        $movie->title = $request->input('title');
+        $movie->rating = $request->input('rating');
+        $movie->running_time = $request->input('runningTime');
+        $movie->website = $request->input('website');
+        $movie->release_date = $request->input('release_date');
+        $movie->trailer = $request->input('trailer');
+        $movie->dvd_release = $request->input('dvdRelease');
+        $movie->synopsys = $request->input('synopsys');
+
+        $movie->save();
+        return response()->json([
+            'title' => 'Vas Movies',
+            'message' => 'Movie updated successfully',
+            'data' => []
+        ]);
     }
 
     public function deleteMovieById($id)
     {
-        // TODO: Implement deleteMovieById() method.
+        $movie = Movies::findOrFail($id);
+        if($movie) {
+            $movie->delete();
+            return response()->json([
+                'title' => 'Vas Movies',
+                'message' => 'Movie deleted successfully',
+                'data' => null
+            ]);
+        }
+
+        return response()->json([
+            'title' => 'Vas Movies',
+            'message' => 'No movie found',
+            'data' => null
+        ], 404);
     }
 }
