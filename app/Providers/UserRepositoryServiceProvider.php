aed919d441790bced39c3f7a71b76039886056be
JosephOlabode <josepholabode17@gmail.com>
2021-03-25 14:03:00 +0000

    user repository service provider implemented

---
+++ app/Providers/UserRepositoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repository\IUser;
+use App\Repository\UserRepository;
+use Illuminate\Support\ServiceProvider;
+
+class UserRepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(IUser::class, UserRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
