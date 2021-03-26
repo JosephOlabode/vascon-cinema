cb382a849ea8b4691dbae8b82f7f006515294d42
JosephOlabode <josepholabode17@gmail.com>
2021-03-26 06:17:14 +0000

    implemented the apiauthcontroller

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
