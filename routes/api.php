13305bc08ebfc7dfc606c0d2944a5986f2ff76e5
JosephOlabode <josepholabode17@gmail.com>
2021-03-26 08:22:48 +0000

    updated the api route file

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     Route::post('/logout', 'Auth\ApiAuthController@logout');
 
     Route::group(['prefix' => 'app/users'], function() {
-        Route::get('', [\App\Http\Controllers\UserController::class, 'update']);
-        Route::get('', [\App\Http\Controllers\UserController::class, 'index']);
-        Route::get('', [\App\Http\Controllers\UserController::class, 'show']);
-        Route::delete('', [\App\Http\Controllers\UserController::class, 'destroy']);
+        Route::patch('/update', [\App\Http\Controllers\UserController::class, 'update']);
+        Route::get('/all', [\App\Http\Controllers\UserController::class, 'index']);
+        Route::get('/user/{id}', [\App\Http\Controllers\UserController::class, 'show']);
+        Route::delete('/user/{id}', [\App\Http\Controllers\UserController::class, 'destroy']);
     });
 
     Route::group([
@@ -41,20 +41,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     Route::group([
         'prefix' => 'app/cinema',
     ], function() {
-        Route::post('', [\App\Http\Controllers\CinemaController::class, 'store']);
-        Route::patch('', [\App\Http\Controllers\CinemaController::class, 'update']);
-        Route::get('', [\App\Http\Controllers\CinemaController::class, 'index']);
-        Route::get('', [\App\Http\Controllers\CinemaController::class, 'show']);
-        Route::delete('', [\App\Http\Controllers\CinemaController::class, 'destroy']);
+        Route::post('/create', [\App\Http\Controllers\CinemaController::class, 'store']);
+        Route::patch('/update', [\App\Http\Controllers\CinemaController::class, 'update']);
+        Route::get('/all', [\App\Http\Controllers\CinemaController::class, 'index']);
+        Route::get('/cinema/{id}', [\App\Http\Controllers\CinemaController::class, 'show']);
+        Route::delete('/cinema/{id}', [\App\Http\Controllers\CinemaController::class, 'destroy']);
     });
 
     Route::group([
         'prefix' => 'app/show-time',
     ], function() {
-        Route::post('', [\App\Http\Controllers\ShowTimeController::class, 'store']);
-        Route::patch('', [\App\Http\Controllers\ShowTimeController::class, 'update']);
-        Route::get('', [\App\Http\Controllers\ShowTimeController::class, 'index']);
-        Route::get('', [\App\Http\Controllers\ShowTimeController::class, 'show']);
-        Route::delete('', [\App\Http\Controllers\ShowTimeController::class, 'destroy']);
+        Route::post('/create', [\App\Http\Controllers\ShowTimeController::class, 'store']);
+        Route::patch('/update', [\App\Http\Controllers\ShowTimeController::class, 'update']);
+        Route::get('/all', [\App\Http\Controllers\ShowTimeController::class, 'index']);
+        Route::get('/show-time/{id}', [\App\Http\Controllers\ShowTimeController::class, 'show']);
+        Route::delete('/show-time/{id}', [\App\Http\Controllers\ShowTimeController::class, 'destroy']);
     });
 });
